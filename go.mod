d1d19f8d9f537b470a518d7bf038056f84710c8e
Julius Liaudanskis <julius.l@emotion.lt>
2021-12-25 23:49:11 +0200

    save images with full url, add channels for saving images, do not panic if failed image decode/encode

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module bitbucket.org/julius_liaudanskis/go-blog
 go 1.17
 
 require (
+	github.com/joho/godotenv v1.4.0
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/justinas/alice v1.2.0
+	golang.org/x/net v0.0.0-20211216030914-fe4d6282115f
 	gorm.io/driver/mysql v1.2.1
 	gorm.io/gorm v1.22.4
 )
@@ -11,8 +15,4 @@ require (
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
-	github.com/joho/godotenv v1.4.0 // indirect
-	github.com/julienschmidt/httprouter v1.3.0 // indirect
-	github.com/justinas/alice v1.2.0 // indirect
-	golang.org/x/net v0.0.0-20211216030914-fe4d6282115f // indirect
 )
--- internal/handlers/images.go
+++ internal/handlers/images.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"bytes"
 	"encoding/base64"
+	"errors"
 	"fmt"
 	"golang.org/x/net/html"
 	"golang.org/x/net/html/atom"
@@ -16,6 +17,11 @@ import (
 	"time"
 )
 
+type imageUploadResult struct {
+	Url   string
+	Error error
+}
+
 func (repo *Repository) parseImageTags(s string) (string, error) {
 	content, err := html.ParseFragment(
 		strings.NewReader(s),
@@ -27,17 +33,20 @@ func (repo *Repository) parseImageTags(s string) (string, error) {
 	)
 	if err != nil {
 		log.Println(err)
+		return "", err
 	}
 	var buf bytes.Buffer
+	ch := make(chan imageUploadResult, 10)
 	for _, node := range content {
 		if node.Type == html.ElementNode && node.Data == "img" {
 			for i, img := range node.Attr {
 				if img.Key == "src" {
-					url, err := repo.saveImages(img.Val)
-					if err != nil {
-						return "", err
+					go repo.saveImages(img.Val, ch)
+					url := <-ch
+					if url.Error != nil {
+						return "", url.Error
 					}
-					node.Attr[i].Val = url
+					node.Attr[i].Val = url.Url
 					break
 				}
 			}
@@ -46,13 +55,16 @@ func (repo *Repository) parseImageTags(s string) (string, error) {
 			return "", err
 		}
 	}
+	close(ch)
 	return buf.String(), nil
 }
 
-func (repo *Repository) saveImages(data string) (string, error) {
+func (repo *Repository) saveImages(data string, ch chan imageUploadResult) {
+	resp := imageUploadResult{Url: "", Error: nil}
 	idx := strings.Index(data, ";base64,")
 	if idx < 0 {
-		panic("InvalidImage")
+		resp.Error = errors.New("invalid image")
+		ch <- resp
 	}
 	ImageType := data[11:idx]
 	rand.Seed(time.Now().UnixNano())
@@ -60,56 +72,68 @@ func (repo *Repository) saveImages(data string) (string, error) {
 	var url string
 	unbased, err := base64.StdEncoding.DecodeString(data[idx+8:])
 	if err != nil {
-		panic("Cannot decode b64")
+		resp.Error = errors.New("cannot decode base64 image")
+		ch <- resp
 	}
 	r := bytes.NewReader(unbased)
 	switch ImageType {
 	case "png":
 		im, err := png.Decode(r)
 		if err != nil {
-			panic("Bad png")
+			resp.Error = errors.New("cannot decode png")
+			ch <- resp
 		}
 		f, err := os.OpenFile(fmt.Sprintf("%s.png", imgName), os.O_WRONLY|os.O_CREATE, 0777)
 		if err != nil {
-			panic("Cannot open file")
+			resp.Error = errors.New("cannot open file")
+			ch <- resp
 		}
 		err = png.Encode(f, im)
 		if err != nil {
-			return "", err
+			resp.Error = errors.New("cannot encode png file")
+			ch <- resp
 		}
 		url = f.Name()
 
 	case "jpeg":
 		im, err := jpeg.Decode(r)
 		if err != nil {
-			panic("Bad jpeg")
+			resp.Error = errors.New("cannot decode jpeg")
+			ch <- resp
 		}
 		f, err := os.OpenFile(fmt.Sprintf("%s.jpeg", imgName), os.O_WRONLY|os.O_CREATE, 0777)
 		if err != nil {
-			panic("Cannot open file")
+			resp.Error = errors.New("cannot decode jpeg file")
+			ch <- resp
 		}
 		err = jpeg.Encode(f, im, nil)
 		if err != nil {
-			return "", err
+			resp.Error = errors.New("cannot encode jpeg file")
+			ch <- resp
 		}
 		url = f.Name()
 
 	case "gif":
 		im, err := gif.Decode(r)
 		if err != nil {
-			panic("Bad gif")
+			resp.Error = errors.New("cannot decode gif")
+			ch <- resp
 		}
 		f, err := os.OpenFile(fmt.Sprintf("%s.gif", imgName), os.O_WRONLY|os.O_CREATE, 0777)
 		if err != nil {
-			panic("Cannot open file")
+			resp.Error = errors.New("cannot open gif file")
+			ch <- resp
 		}
 		err = gif.Encode(f, im, nil)
 		if err != nil {
-			return "", err
+			resp.Error = errors.New("cannot encode gif")
+			ch <- resp
 		}
 		url = f.Name()
 	}
-	return url, nil
+	fullUrl := fmt.Sprintf("http://%s/%s", os.Getenv("APP_BASE_URL"), url)
+	resp.Url = fullUrl
+	ch <- resp
 }
 
 func randSeq(n int) string {
