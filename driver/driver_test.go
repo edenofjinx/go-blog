e7534f986c4e622a4f14167b7560681589ae10e5
Julius Liaudanskis <julius.l@emotion.lt>
2022-01-17 23:34:22 +0200

    improved driver tests

--- driver/driver_test.go
+++ driver/driver_test.go
@@ -3,21 +3,73 @@ package driver
 import (
 	"bitbucket.org/julius_liaudanskis/go-blog/config"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"gorm.io/gorm"
+	"log"
+	"os"
 	"testing"
 )
 
-var testDsn = "root:root@tcp(localhost:3306)/go_blog_test?charset=utf8&parseTime=True&loc=Local"
-var a config.AppConfig
-
-func TestConnectSQL(t *testing.T) {
-	d, err := ConnectSQL(testDsn, a)
-	assert.Nil(t, err)
-	assert.ObjectsAreEqualValues(gorm.DB{}, d)
+type driverTestSuite struct {
+	suite.Suite
+	a config.AppConfig
 }
 
-func TestNewDatabase(t *testing.T) {
-	d, err := NewDatabase(testDsn, a)
-	assert.Nil(t, err)
-	assert.ObjectsAreEqualValues(gorm.DB{}, d)
+var testsForDatabaseConnection = []struct {
+	name          string
+	dsn           string
+	expectedError bool
+}{
+	{
+		name:          "valid connection",
+		dsn:           "root:root@tcp(localhost:3306)/go_blog_test?charset=utf8&parseTime=True&loc=Local",
+		expectedError: false,
+	},
+	{
+		name:          "invalid connection",
+		dsn:           "fail:test@tcp(localhost:3306)/go_blog_test?charset=utf8&parseTime=True&loc=Local",
+		expectedError: true,
+	},
+}
+
+func (suite *driverTestSuite) SetupSuite() {
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app := config.AppConfig{
+		InfoLog:      infoLog,
+		ErrorLog:     errorLog,
+		InProduction: false,
+		AppVersion:   "test",
+		Environment:  "test",
+		StaticImages: "static/test/images/",
+	}
+	suite.a = app
+}
+
+func TestMysqlTestSuite(t *testing.T) {
+	suite.Run(t, new(driverTestSuite))
+}
+
+func (suite *driverTestSuite) TestConnectSQL() {
+	for _, t := range testsForDatabaseConnection {
+		d, err := ConnectSQL(t.dsn, suite.a)
+		if err != nil {
+			if t.expectedError == false {
+				suite.Fail("did not expect a database connection error")
+			}
+		}
+		assert.ObjectsAreEqualValues(gorm.DB{}, d)
+	}
+}
+
+func (suite *driverTestSuite) TestNewDatabase() {
+	for _, t := range testsForDatabaseConnection {
+		d, err := NewDatabase(t.dsn, suite.a)
+		if err != nil {
+			if t.expectedError == false {
+				suite.Fail("did not expect a database connection error")
+			}
+		}
+		assert.ObjectsAreEqualValues(gorm.DB{}, d)
+	}
 }
